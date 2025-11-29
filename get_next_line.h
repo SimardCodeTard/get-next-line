edeb0b4664fc93ea5819c9b12fd1694dfc27ebcb
smenard <floogles71@gmail.com>
2025-11-29 11:55:00 +0100

    feat(*): optimize buffer start search

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 11:29:37 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/28 16:04:05 by smenard          ###   ########.fr       */
+/*   Updated: 2025/11/29 12:22:48 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 128
+#  define BUFFER_SIZE 256
 # endif
 
 # ifndef MAX_FD
@@ -27,6 +27,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdint.h>
 
 typedef enum e_line_build_status
 {
@@ -57,6 +58,8 @@ char					*get_next_line(int fd);
 char					*build_next_line(int fd, t_buffer *buffer);
 
 t_line_build_status		extract_line(int fd, t_list *lst, t_buffer *buffer);
+
+size_t					find_buffer_start(t_buffer *buffer);
 
 t_buffer				*get_buffer(int fd);
 
